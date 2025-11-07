9dbf5592d2754cc04a93cbc4131da25ab34c9d13
444outlan <heyfuyar@outlook.com>
2025-11-06 21:37:44 -0600

    Add initial IXL Cheat user script

---
+++ ixl-cheat.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         IXL Cheat (test)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  testing github pages hosting
+// @match        *://*.ixl.com/*
+// @grant        none
+// ==/UserScript==
+
+console.log("✅ script from GitHub Pages loaded!");
+alert("IXL-Cheat test script loaded successfully!");
